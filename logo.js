c0d283bcf2328c266fbdc1fb7b805d45eaa3ccd4
Mark Womack <mwomack@pcoe.k12.ca.us>
2019-11-11 09:11:58 -0800

    adding lots of stuff

--- logo.js
+++ logo.js
@@ -27,19 +27,28 @@ constructor(x,y,as){this.xs=x;this.ys=y;this.xf=x;this.yf=y;this.angle=as;}
 	  if(this.angle>=360){this.angle=this.angle%360;}
 	 }
 
+push(){this.xp=this.xs;this.yp=this.ys;
+       this.anglep=this.angle;}
+pop(){this.xs=this.xp;this.ys=this.yp;
+      this.angle=this.anglep;
+     }
+
  xpos(){return this.xf;}
  ypos(){return this.yf;}
  dir(){return this.angle;}
 
-sketch(l,a,senten){
+sketch(l,a,senten,factor){
+l=l*factor;
 let news="";//sentence="A+A--A+A";
   for(let j=0;j<senten.length;j++){
     news=senten.charAt(j);
     switch(news){
       case "A":this.fd(l);break;
-      case "B":this.fd(l);break;
+      case "G":this.fd(l);break;
       case "+":this.lt(a);break;
       case "-":this.rt(a);break;
+	  case "[":this.push();break;
+      case "]":this.pop();break;
 }
   }}
   
