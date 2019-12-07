9f92b04b93a388512dd1463c19dd98fc4dc9215d
Mark Womack <mwomack@pcoe.k12.ca.us>
2019-12-07 06:54:09 -0800

    just making changes

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 let t;
-let ax="A++A++A++";
-//let ax="A";
+//let ax="A++A++A++";
+let ax="A";
 let str=ax;
 let sentF= "A+G--G+A";let sentG="A+A--G++A-A"
+let sentB="B";
 //let sentF="+G-A-G+";let sentG="-A+G+A-";
 let xm=50,ym=200;
 let len;let an=60;let ycor=30;
@@ -13,13 +14,15 @@ function setup(){
 t=new logo(50,200,0);
 len=250;
 background(200,100,0);
-factor=0.34
+factor=0.666667;
 }
 
 function draw() {
 background(220,100,0);
+ellipse(mouseX,mouseY,10,10);
 text(" length is " +len+ " angle is "+an+ " and factor is "+factor,20,20);
 t.set1(xm,ym,0);word(t,len,an,str);
+text(str,30,50);
 }
 
 function mousePressed(){
--- stuff.js
+++ stuff.js
@@ -1,11 +1,18 @@
-function chgAll(){
+/*function chgAll(){
 let strAr=[];
 let string1=document.getElementById("inpall").value;
   strAr=string1.split(',');
   ax=strAr[0];
-  sentF=strAr[1];sentG=strAr[2];
+  sentF=strAr[1];sentG=strAr[2];sentB=strAr[3];
  str=ax;
   t.angle=0;
+}*/
+
+function chgAll(){
+	let strAr=[];
+	let string2=document.getElementById("inpall").value;
+    strAr=string2.split(',');sentF=strAr[0];
+	str=strAr[1];ax=str;
 }
 
 function length(){
@@ -14,20 +21,27 @@ let string1=document.getElementById("inplen").value;
   strAr=string1.split(',');//str="";
   len=strAr[0];an=strAr[1];
   factor=strAr[2];
-  ycor+=10;t.angle=0;
+  
 }
 function sierTri(){
 	ax="A";len=200;factor=0.6667;
-	sentF="+G-A-G+"; sentG="-A+G+A-";an=60.0
+	sentF="+G-A-G+"; sentG="-A+G+A-";sentB="";
+	an=60.0
 	str=ax;
 }
 
 function koch(){
 	ax="A--A--A--";len=200;factor=0.66667;
-	sentF="A+A--A+A"; sentG="G";an=60.0
+	sentF="A+A--A+A"; sentG="G";sentB="B";
+	an=60.0
 	str=ax;
 }
-
+ function pp(){
+	 ax="A+[AB]--[AB]-B";len=200;factor=0.666667;
+	 //sentF="A";	 
+	 sentF="A+[AB]--[AB]-B";
+	 sentG="G";str=ax;sentB="B";
+ }
 
 function dir1(){
 alert("incomplete at moment.  example click sier then click on screen. then click generate, repeat.  to help fit, click length bar.  to change structure, click angle bar. ");
@@ -43,12 +57,12 @@ function gen(){//generates new lindel string
 function reStart(){
 	t.angle=0; str= "A";ax="A";len=200;
 	sentF="A+GA--A";sentG="G-GA++G-";factor=.666667;
-		an=95;
+		an=60;sentB="B";
 		}
 		
 function info(){
-let infoNow=ax+" "+sentF+" "+sentG+" "+len+" "+an+" "+factor+"  "+str;	
-alert(str);
+let infoNow="ax "+ax+" sentF "+sentF+" sentG "+sentG+" sentB "+sentB+" len "+len+" an "+an+" factor  "+factor+" str "+str;	
+alert(infoNow);
 }
 
 function chgLen(){
@@ -57,6 +71,7 @@ len=document.getElementById("range1").valueAsNumber;
 
 function chgAng(){
 an=document.getElementById("range2").valueAsNumber;	
+an=floor(an);
 }
 
 
@@ -67,8 +82,10 @@ let news='';
     news=senten[j];
     if(news=='A'){t.fd(l);}
     if(news=='G'){t.fd(l);}
-	if(news=='+'){t.lt(a);}
-    if(news=='-'){t.rt(a);}
+	if(news=='B'){t.bk(l);}
+    if(news=='f'){len=factor*len;}
+    if(news=='-'){t.lt(a);}
+    if(news=='+'){t.rt(a);}
     if(news=='['){t.push();}
 	if(news==']'){t.pop();}
 }}
@@ -81,6 +98,8 @@ ch=sent[j];
   switch(ch){
 	case 'A':newsent=newsent+sentF;break;
 	case 'G':newsent=newsent+sentG;break;
+	case 'B':newsent=newsent+sentB;break;
+	case 'f':newsent=newsent+"f";break;
 	case '+':newsent=newsent+"+";break;
 	case '-':newsent=newsent+"-";break;
 	case '[':newsent=newsent+"[";break;
